12c03d75990e102092fdc47ba7e2a908deb01d80
Daniel Silva <dnlwillian@gmail.com>
2025-07-02 16:04:12 -0300

    chore(building blocks): base de entidades entity framework (ref #19)

---
+++ backend/buildingblocks/BM.BuildingBlocks.Domain/Base/Entity.cs
@@ -0,0 +1,14 @@
+﻿using BM.BuildingBlocks.Domain.Abstractions;
+
+namespace BM.BuildingBlocks.Domain.Base
+{
+    public abstract class Entity : IEntity
+    {
+        public Guid Id { get; set; }
+
+        private readonly List<IDomainEvent> _eventosDominio = new();
+        public IReadOnlyCollection<IDomainEvent> ObterEventosDominio() => _eventosDominio;
+        public void AdicionarEventoDominio(IDomainEvent evento) => _eventosDominio.Add(evento);
+        public void LimparEventosDominio() => _eventosDominio.Clear();
+    }
+}
