2fd6b4be9078a58461384571bc85641d8c72328b
Daniel Silva <dnlwillian@gmail.com>
2025-07-02 16:05:09 -0300

    chore(building blocks): base de integração com eventos (ref #19)

---
+++ backend/buildingblocks/BM.BuildingBlocks.Domain/Events/Base/IntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BM.BuildingBlocks.Domain.Events.Base;
+
+public abstract class IntegrationEvent
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public DateTime OccurredAt { get; init; } = DateTime.UtcNow;
+    public string? EventType => GetType().Name;
+}
---
+++ backend/buildingblocks/BM.BuildingBlocks.Domain/Events/Base/IntegrationEventEnvelope.cs
@@ -0,0 +1,14 @@
+﻿namespace BM.BuildingBlocks.Domain.Events.Base;
+
+public class IntegrationEventEnvelope<T> where T : IntegrationEvent
+{
+    public string CorrelationId { get; set; }
+    public string EventType => typeof(T).Name;
+    public T Data { get; init; }
+
+    public IntegrationEventEnvelope(T data, string correlationId)
+    {
+        Data = data;
+        CorrelationId = correlationId;
+    }
+}
