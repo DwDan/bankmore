6e70069a3b9427a31b0924d2e4f3ee79ce1d3368
Daniel Silva <dnlwillian@gmail.com>
2025-07-02 16:25:01 -0300

    chore(building blocks): base controller (ref #19)

---
+++ backend/buildingblocks/BM.BuildingBlocks.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using BM.BuildingBlocks.Core.Exception;
+using BM.BuildingBlocks.WebAPI.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BM.BuildingBlocks.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected IActionResult Ok<T>(T data) =>
+                base.Ok(new ApiResponseWithData<T> { Data = data, Success = true });
+
+        protected void EnsureRouteMatchesBodyId(Guid routeId, Guid bodyId)
+        {
+            if (routeId != bodyId)
+                throw new BadRequestException("Error.RequestBodyIdMismatch");
+        }
+    }
+}
