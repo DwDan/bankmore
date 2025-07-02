f29cfe1e0a6aa72ee9fd495724ddfdcde1269856
Daniel Silva <dnlwillian@gmail.com>
2025-07-02 16:15:40 -0300

    chore(building blocks): classes concretas de autenticação e segurança (ref #19)

---
+++ backend/buildingblocks/BM.BuildingBlocks.Infrastructure/Security/BCryptPasswordHash.cs
@@ -0,0 +1,17 @@
+﻿using BM.BuildingBlocks.Domain.Security;
+
+namespace BM.BuildingBlocks.Infrastructure.Security
+{
+    public class BCryptPasswordHash : IPasswordHash
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+    }
+}
---
+++ backend/buildingblocks/BM.BuildingBlocks.Infrastructure/Security/JwtTokenGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using BM.BuildingBlocks.Domain.Security;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BM.BuildingBlocks.Infrastructure.Security;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtTokenGenerator(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    private string GenerateTokenInternal(IEnumerable<Claim> claims, DateTime expires, string secret)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(secret);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = expires,
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+
+    public string GenerateToken(IUsuario user)
+    {
+        var secretKey = _configuration["Jwt:SecretKey"];
+        ArgumentException.ThrowIfNullOrWhiteSpace(secretKey);
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim("Cpf", user.Cpf),
+        };
+
+        return GenerateTokenInternal(claims, DateTime.UtcNow.AddHours(8), secretKey);
+    }
+}
